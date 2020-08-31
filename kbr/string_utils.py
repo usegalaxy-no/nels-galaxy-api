b3864426a0d033b0b78bfac4ad4e2e2fc9cf1943
brugger <kim.brugger@uib.no>
2020-08-31 13:09:28 +0200

    cleanup

--- kbr/string_utils.py
+++
@@ -1,8 +0,0 @@
-
-email_match = '([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})'
-email_regex = r"^{}$".format(email_match)
-
-domain_match = '([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})'
-
-def comma_sep(elements:[]) -> str:
-    return ", ".join( map(str, elements))
